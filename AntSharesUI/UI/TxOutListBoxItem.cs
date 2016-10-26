7a19b305fb7a48ff20db67d1492e37858679730f
Erik Zhang <erik@vcage.com>
2016-10-27 03:44:41 +0800

    split from antshares

---
+++ AntSharesUI/UI/TxOutListBoxItem.cs
@@ -0,0 +1,16 @@
+﻿using AntShares.Core;
+using AntShares.Wallets;
+
+namespace AntShares.UI
+{
+    internal class TxOutListBoxItem
+    {
+        public TransactionOutput Output;
+        public string AssetName;
+
+        public override string ToString()
+        {
+            return $"{Wallet.ToAddress(Output.ScriptHash)}\t{Output.Value}\t{AssetName}";
+        }
+    }
+}
