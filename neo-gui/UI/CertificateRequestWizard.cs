56a8b0267e5869dce72411b46926cd7dda6aa487
PeterLinX <18610565179@163.com>
2017-10-02 00:28:44 +0800

    update

--- neo-gui/UI/CertificateRequestWizard.cs
+++ neo-gui/UI/CertificateRequestWizard.cs
@@ -27,27 +27,27 @@ namespace Neo.UI
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //if (saveFileDialog1.ShowDialog() != DialogResult.OK) return;
-            //ECPoint point = (ECPoint)comboBox1.SelectedItem;
-            //KeyPair key = Program.CurrentWallet.GetKey(point);
-            //byte[] pubkey = point.EncodePoint(false).Skip(1).ToArray();
-            //byte[] prikey;
-            //using (key.Decrypt())
-            //{
-            //    const int ECDSA_PRIVATE_P256_MAGIC = 0x32534345;
-            //    prikey = BitConverter.GetBytes(ECDSA_PRIVATE_P256_MAGIC).Concat(BitConverter.GetBytes(32)).Concat(pubkey).Concat(key.PrivateKey).ToArray();
-            //}
-            //CX509PrivateKey x509key = new CX509PrivateKey();
-            //x509key.AlgorithmName = "ECDSA_P256";
-            //x509key.Import("ECCPRIVATEBLOB", Convert.ToBase64String(prikey));
-            //Array.Clear(prikey, 0, prikey.Length);
-            //CX509CertificateRequestPkcs10 request = new CX509CertificateRequestPkcs10();
-            //request.InitializeFromPrivateKey(X509CertificateEnrollmentContext.ContextUser, x509key, null);
-            //request.Subject = new CX500DistinguishedName();
-            //request.Subject.Encode($"CN={textBox1.Text},C={textBox2.Text},S={textBox3.Text},SERIALNUMBER={textBox4.Text}");
-            //request.Encode();
-            //File.WriteAllText(saveFileDialog1.FileName, "-----BEGIN NEW CERTIFICATE REQUEST-----\r\n" + request.RawData + "-----END NEW CERTIFICATE REQUEST-----\r\n");
-            //Close();
+            if (saveFileDialog1.ShowDialog() != DialogResult.OK) return;
+            ECPoint point = (ECPoint)comboBox1.SelectedItem;
+            KeyPair key = Program.CurrentWallet.GetKey(point);
+            byte[] pubkey = point.EncodePoint(false).Skip(1).ToArray();
+            byte[] prikey;
+            using (key.Decrypt())
+            {
+                const int ECDSA_PRIVATE_P256_MAGIC = 0x32534345;
+                prikey = BitConverter.GetBytes(ECDSA_PRIVATE_P256_MAGIC).Concat(BitConverter.GetBytes(32)).Concat(pubkey).Concat(key.PrivateKey).ToArray();
+            }
+            CX509PrivateKey x509key = new CX509PrivateKey();
+            x509key.AlgorithmName = "ECDSA_P256";
+            x509key.Import("ECCPRIVATEBLOB", Convert.ToBase64String(prikey));
+            Array.Clear(prikey, 0, prikey.Length);
+            CX509CertificateRequestPkcs10 request = new CX509CertificateRequestPkcs10();
+            request.InitializeFromPrivateKey(X509CertificateEnrollmentContext.ContextUser, x509key, null);
+            request.Subject = new CX500DistinguishedName();
+            request.Subject.Encode($"CN={textBox1.Text},C={textBox2.Text},S={textBox3.Text},SERIALNUMBER={textBox4.Text}");
+            request.Encode();
+            File.WriteAllText(saveFileDialog1.FileName, "-----BEGIN NEW CERTIFICATE REQUEST-----\r\n" + request.RawData + "-----END NEW CERTIFICATE REQUEST-----\r\n");
+            Close();
         }
     }
 }
