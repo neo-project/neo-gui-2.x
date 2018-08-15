c6f07888281092aa6fba33dbfda3206483421bad
陈志同 <chenzhitong12@hotmail.com>
2018-08-15 15:57:20 +0800

    Fix a bug with a precision error in the transaction output on DevTools dialog. (#255)
    * Fix a bug with a precision error in the transaction output on DevTools dialog.
    
    * Fix bugs in neo-gui developer tools that cannot load and edit contract parameters

--- neo-gui/UI/DeveloperToolsForm.ContractParameters.cs
+++ neo-gui/UI/DeveloperToolsForm.ContractParameters.cs
@@ -1,8 +1,10 @@
+using Neo.Core;
 using Neo.Network;
 using Neo.Properties;
 using Neo.SmartContract;
 using Neo.Wallets;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 
@@ -18,7 +20,22 @@ namespace Neo.UI
             listBox2.Items.Clear();
             if (Program.CurrentWallet == null) return;
             UInt160 hash = Wallet.ToScriptHash((string)listBox1.SelectedItem);
+            var parameters = context.GetParameters(hash);
+            if (parameters == null)
+            {
+                var parameterList = Program.CurrentWallet.GetAccount(hash).Contract.ParameterList ?? Blockchain.Default.GetContract(hash).ParameterList;
+                if (parameterList != null)
+                {
+                    var pList = new List<ContractParameter>();
+                    for (int i = 0; i < parameterList.Length; i++)
+                    {
+                        pList.Add(new ContractParameter(parameterList[i]));
+                        context.Add(Program.CurrentWallet.GetAccount(hash).Contract, i, null);
+                    }
+                }
+            }
             listBox2.Items.AddRange(context.GetParameters(hash).ToArray());
+            button4.Visible = context.Completed;
         }
 
         private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
@@ -70,8 +87,16 @@ namespace Neo.UI
         {
             context.Verifiable.Scripts = context.GetScripts();
             IInventory inventory = (IInventory)context.Verifiable;
-            Program.LocalNode.Relay(inventory);
-            InformationBox.Show(inventory.Hash.ToString(), Strings.RelaySuccessText, Strings.RelaySuccessTitle);
+            var tx = inventory as Transaction;
+            if (tx.Verify(new List<Transaction> { tx }))
+            {
+                Program.LocalNode.Relay(inventory);
+                InformationBox.Show(inventory.Hash.ToString(), Strings.RelaySuccessText, Strings.RelaySuccessTitle);
+            }
+            else
+            {
+                MessageBox.Show("Transaction validation failed and cannot be broadcast.");
+            }
         }
     }
 }
